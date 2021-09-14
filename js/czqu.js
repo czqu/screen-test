7441bbd1671ed50fca4453a079d90640c50c1f27
陈子琦 <Ryan.h.qin@qq.com>
2021-09-14 23:57:05 +0800

    init

---
+++ js/czqu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?202567f05f85026a62a117eec19b4a09";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
